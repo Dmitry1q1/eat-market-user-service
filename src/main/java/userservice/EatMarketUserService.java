91d45a2fb8b3f320577381a8f16f22ed29caddcd
TotsSamuy <72630727+TotsSamuy@users.noreply.github.com>
2022-01-28 01:58:45 +0300

    delete test  logging in main

--- src/main/java/userservice/EatMarketUserService.java
+++ src/main/java/userservice/EatMarketUserService.java
@@ -7,12 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EatMarketUserService {
 
-    static Logger logger = Logger.getLogger(EatMarketUserService.class.getName());
-
     public static void main(String[] args) {
-        logger.info("Start app");
-        logger.debug("Start app");
-        logger.error("Start app");
         SpringApplication.run(EatMarketUserService.class, args);
     }
 }
